9a8ab929675e4dce7dc4ecd654725d9d4b8fb3c0
Florent Captier <florent@captier.org>
2017-03-27 17:58:40 +0200

    Add information for PyPi

--- setup.py
+++ setup.py
@@ -5,12 +5,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='denonavr3312',
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(),
-    url='',
-    license='',
+    url='https://github.com/gyzpunk/denonavr3312',
+    license='GNU',
     author='Florent Captier',
     author_email='florent@captier.org',
     description='Control Denon AVR 3312 through telnet interface',
-    tests_require=['tox', 'pytest']
+    tests_require=['tox', 'pytest'],
+    platforms=['any'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Home Automation",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ]
 )
